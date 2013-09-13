c9b3a1d414f10ac68855cd0af1da5b9900039f53
foxbupt <supervisedboy@gmail.com>
2013-09-13 23:00:16 +0800

    commit

--- web/modules/stock/models/StockPool.php
+++ web/modules/stock/models/StockPool.php
@@ -61,6 +61,7 @@ class StockPool extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+            'stock' => array(self::BELONGS_TO, 'Stock', 'sid'),
 		);
 	}
 
