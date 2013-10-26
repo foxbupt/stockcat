ce0e5a394cd950a22490b820c3857b7c4e56bff1
foxbupt <supervisedboy@gmail.com>
2013-10-26 17:09:38 +0800

    commit

--- web/data/sql/db_member.sql
+++ web/data/sql/db_member.sql
@@ -12,10 +12,10 @@ CREATE TABLE IF NOT EXISTS `t_account`
 	`gender`    char(1) NOT NULL default 'M' COMMENT '性别: M 男, F 女',	
 	`intro`     varchar(255) NOT NULL default '' COMMENT '简介',
 	`mobile_no` varchar(16) NOT NULL default '' COMMENT '手机号',
-	`birthday`  int(11) NOT NULL default '' COMMENT '出生日期',
+	`birthday`  int(11) NOT NULL default 0 COMMENT '出生日期',
 	`active_code` varchar(32) NOT NULL default '' COMMENT '账号激活码',
     `referer_code` varchar(32) NOT NULL default '' COMMENT '账号邀请注册码',
-    `register_ip` int(11) NOT NULL default 0 COMMENT '注册ip',  
+    `register_ip`  int(11) unsigned NOT NULL default 0 COMMENT '注册ip',  
 	`update_time` int(11) NOT NULL default 0 COMMENT '最近修改时间',
     `create_time` int(11) NOT NULL default 0 COMMENT '注册时间',
     `status`    enum('Y', 'U', 'N') default 'Y' COMMENT '账号状态取值: U 未激活, Y 已激活, N 禁用',
@@ -49,17 +49,19 @@ CREATE TABLE IF NOT EXISTS `t_open_bind`
 /**
  * 用户持有股票: 由于单只股票可以间隔买入多次, 因此需要用batch_no批次来建立持有记录与交易记录的关联关系
  */
-CREATE TABLE IF NOT EXISTS `t_user_own`
+CREATE TABLE IF NOT EXISTS `t_user_hold`
 (
 	`id`        int(11) unsigned NOT NULL AUTO_INCREMENT,
-	`batch_no`  int(11) unsigned NOT NULL default 0 COMMENT '批次编码, 格式为YYYYmmddHH',
+	`batch_no`  int(11) unsigned NOT NULL default 0 COMMENT '批次编码, 格式为YYYYmmdd',
 	`uid`       int(11) NOT NULL default 0 COMMENT '用户uid',
 	`sid`       int(11) NOT NULL default 0 COMMENT '股票id',
 	`count`     int(11) NOT NULL default 0 COMMENT '持有数量',
-    `cost`      decimal(6,2) NOT NULL default 0 COMMENT '每股买入成本单价',
+    `price`     decimal(6,2) NOT NULL default 0 COMMENT '每股买入成本单价',
+    `cost`      decimal(10,2) NOT NULL default 0 COMMENT '买入成本',
 	`amount`    decimal(10,2) NOT NULL default 0 COMMENT '当前市值',
     `profit`    decimal(10,2) NOT NULL default 0 COMMENT '收益总额',
     `profit_portion` decimal(6,2) NOT NULL default 0 COMMENT '收益比例',
+    `update_time` int(11) NOT NULL default 0 COMMENT '最近修改时间',
 	`create_time` int(11) NOT NULL default 0 COMMENT '创建时间',
     `status`    enum('Y', 'N') default 'Y',
 	
@@ -77,14 +79,15 @@ CREATE TABLE IF NOT EXISTS `t_user_deal`
 	`batch_no`  int(11) NOT NULL default 0 COMMENT '批次编码, 用于关联股票持有记录',
 	`uid`       int(11) NOT NULL default 0 COMMENT '用户uid',
 	`sid`       int(11) NOT NULL default 0 COMMENT '股票id',
-	`day`       int(11) NOT NULL default 0 COMMENT '交易日',
-	`time`      int(11) NOT NULL default 0 COMMENT '交易时刻',
+	`day`       int(11) NOT NULL default 0 COMMENT '交易日期',
     `deal_type` tinyint(1) NOT NULL default 0 COMMENT '交易类型: 1 买入, 2 卖出',
 	`count`     int(11) NOT NULL default 0 COMMENT '交易数量',
 	`price`     decimal(6,2) NOT NULL default 0 COMMENT '交易价格',
 	`fee`       decimal(10,2) NOT NULL default 0.0 COMMENT '交易金额',
-	`tax`       decimal(10,2) NOT NULL default 0.0 COMMENT '印花税及费用',
+	`commission` decimal(10,2) NOT NULL default 0.0 COMMENT '佣金费用',
+	`tax`       decimal(10,2) NOT NULL default 0.0 COMMENT '税率费用',
 	`amount`    decimal(10,2) NOT NULL default 0.0 COMMENT '总金额',
+	`remark`    varchar(1024) NOT NULL default '' COMMENT '备注信息',    
 	`create_time` int(11) NOT NULL default 0 COMMENT '创建时间',
     `status`    enum('Y', 'N') default 'Y',
 	
