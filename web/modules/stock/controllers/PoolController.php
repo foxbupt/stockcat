e3dfc838b5550b1630e648becfbd074c8252602c
foxbupt <supervisedboy@gmail.com>
2014-05-13 14:02:14 +0800

    commit

---
+++ web/modules/stock/controllers/PoolController.php
@@ -0,0 +1,179 @@
+<?php
+
+/**
+ * @desc 关注股票池
+ * @author fox
+ * @date 2014/05/12
+ *
+ */
+class PoolController extends Controller 
+{
+    /**
+     * @desc 股票池列表实时刷新展示
+     * @param $_GET['day'] 可选
+     */
+    public function actionIndex()
+    {
+        // TODO: 目前查询上一个有效交易日的连续上涨/价格突破历史和年内新高的股票列表, 后续统一查询股票池列表
+        $day = isset($_GET['day'])? intval($_GET['day']) : intval(date('Ymd'));
+        $lastDay = CommonUtil::getPastOpenDay($day, 1);
+        // var_dump($day, $lastDay);
+
+        $sidList = array();
+
+        $contMap = array();
+        $contList = StockPool::model()->findAll(array(
+                                'condition' => "day = ${lastDay} and status = 'Y'",
+                                'order' => 'sum_price_vary_portion desc, day asc',
+                             ));
+        foreach ($contList as $record)
+        {
+            $sidList[] = $record->sid;
+            $contMap[$record->sid] = $record->getAttributes();
+        }
+
+        $priceMap = array();
+        $priceList = StockPriceThreshold::model()->findAll(array(
+                                            'condition' => "day = {$lastDay} and (high_type = 1 or high_type = 2) and status = 'Y'",
+                                        ));
+        foreach ($priceList as $record)
+        {
+            $sidList[] = $record->sid;
+            $priceMap[$record->sid] = $record->getAttributes();
+        }
+
+        $sidList = array_unique($sidList);
+        $key = "poollist-" . $day;
+        $sidMap = Yii::app()->redis->get($key);
+        // var_dump($sidList, $sidMap);
+
+        // 设置到redis缓存中, 用于后台获取开盘价和实时价格
+        if (empty($sidMap))
+        {
+            $sidMap = array();
+
+            foreach ($sidList as $sid)
+            {
+                $stockInfo = StockUtil::getStockInfo($sid);
+                if (!empty($stockInfo))
+                {
+                    $sidMap[$sid] = strtolower($stockInfo['ecode'] . $stockInfo['code']);
+                }
+            }
+
+            Yii::app()->redis->set($key, json_encode($sidMap), 86400);
+        }
+
+        $hqDataMap = array();
+        $curTime = 0;
+        foreach ($sidList as $sid)
+        {
+            $hqDataMap[$sid] = self::getPoolHQData($sid, $day, $lastDay);
+            $curTime = $hqDataMap[$sid]['cur_time'];
+        }
+
+        $this->render('index', array(
+                    'sidList' => $sidList,
+                    'contMap' => $contMap,
+                    'priceMap' => $priceMap,
+                    'hqMap' => $hqDataMap,
+                    'day' => $day,
+                    'lastDay' => $lastDay,
+                    'curTime' => $curTime,
+                ));
+    }
+
+    // 获取关注股票池中股票的行情数据
+    public static function getPoolHQData($sid, $day, $lastDay)
+    {
+        $hqData = array('detail' => array());
+
+        $stockInfo = StockUtil::getStockInfo($sid);
+        $hqData['stock'] = $stockInfo;
+        $hqData['data'] = $stockData = StockData::model()->findByAttributes(array('sid' => $sid, 'day' => $lastDay, 'status' => 'Y'));
+        $closePrice = (float)$stockData['close_price'];
+
+        $dailyKey = "daily-" . $sid . "-" . $day;
+        $cacheValue = Yii::app()->redis->get($dailyKey);
+        $curTime = intval(date('Hi'));
+
+        if ($cacheValue)
+        {
+            $dailyData = json_decode($cacheValue, true);
+            // var_dump($dailyData);
+            $hqData['detail'] = $dailyData;
+            $curTime = $dailyData['time'][count($dailyData['time']) - 1];
+
+            $priceList = $dailyData['price'];
+            $hqData['open_price'] = (float)$priceList[0];
+            $hqData['cur_price'] = (float)$priceList[count($priceList) - 1];
+            
+            if (count($priceList) > 2)
+            {
+                $hqData['trend'] = self::getTrend($hqData['open_price'], $priceList);
+            }
+            // var_dump($hqData);
+        }
+
+        $hqData['cur_time'] = sprintf("%02d:%02d", $curTime/100, $curTime%100);
+        return $hqData;
+    }
+
+    public static function getTrend($openPrice, $priceList)
+    {
+        $trendInfo = array();
+
+        $maxPrice = max($priceList);
+        $maxIndex = array_search($maxPrice, $priceList);
+        $minPrice = min($priceList);
+        $minIndex = array_search($minPrice, $priceList);
+        $curPrice = $priceList[count($priceList) - 1]; 
+        $maxVary = $maxPrice - $curPrice;
+        $minVary = $curPrice - $minPrice;
+
+        if ($curPrice > $openPrice) // 上涨
+        {
+            if ($curPrice == $maxPrice) 
+            {
+                $trendInfo['trend'] = "上涨";
+            }    
+            else 
+            {
+                $varyPortion = ($curPrice - $openPrice) / $openPrice;
+                if ($varyPortion < 0.01)
+                {
+                    $trendInfo['trend'] = "震荡";
+                }
+                else 
+                {
+                    $trendInfo['trend'] = ($maxVary >= $minVary)? "下跌" : "上涨";
+                }
+            }
+        }
+        else if ($curPrice == $openPrice)
+        {
+            $trendInfo['trend'] = "震荡";
+        }
+        else // 下跌
+        {
+            $varyPortion = abs(($openPrice - $curPrice) / $openPrice);
+            $trendInfo['trend'] = ($varyPortion >= 0.01)? "下跌": "震荡下跌";
+        }
+
+        if (strstr($trendInfo['trend'], "震荡") !== FALSE)
+        {
+            $trendInfo['op'] = "待定";
+        }
+        else if ($trendInfo['trend'] == "上涨")
+        {
+            $trendInfo['op'] = "买入";
+        }
+        else
+        {
+            $trendInfo['op'] = "卖出";
+        }
+
+        return $trendInfo;
+    }
+}
+?>
--- web/modules/stock/views/pool/index.php
+++ web/modules/stock/views/pool/index.php
@@ -1,3 +1,13 @@
+<style>
+.red {
+    color: #ff0000;
+}
+.green {
+    color: #008000;
+}
+    
+</style>
+
 <div class="container">
 	<div class="span10">
 		<div class="hd">
@@ -9,7 +19,7 @@
 				<a class="btn btn-primary" type="button" href="<?php echo $this->createUrl('/stock/stock/add');?>">添加股票</a>
 			</p>
 			<table class="table table-bordered">
-				<caption>当前共有<strong><?php echo count($sidList); ?></strong>支关注股票</caption>
+            <caption>当前共有<strong><?php echo count($sidList); ?></strong>支关注股票, 当前时刻: <?php echo $curTime; ?></caption>
 				<thead>
 					<tr>
 						<th>名称</th>
@@ -20,20 +30,21 @@
 						<th>价格突破</th>
 						<th>昨收</th>
 						<th>今开</th>
-						<th>9:40价格</th>
-						<th>开盘走势</th>
+						<th>当前价格</th>
 						<th>涨跌幅</th>
+						<th>开盘走势</th>
 						<th>建议操作</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php foreach ($sidList as $sid): ?>
-                    <?php $stockInfo = StockUtil::getStockInfo($sid); ?>
-                    <?php $stockData = StockData::model()->findByAttributes(array('sid' => $sid, 'day' => $lastDay, 'status' => 'Y')); ?>
+                    <?php $stockInfo = $hqMap[$sid]['stock']; ?>
+                    <?php $stockData = $hqMap[$sid]['data']; ?>
+                    <?php $hqData = $hqMap[$sid]['detail']; ?>
                     <?php $highTypeValues = CommonUtil::getConfigObject("price.high_type"); ?>
                     <?php $qqhqUrl = "http://stockhtm.finance.qq.com/sstock/ggcx/" . $stockInfo['code'] . ".shtml"; ?>
 
-					<tr>
+					<tr class="pull-center">
                         <td><?php echo $stockInfo['name']; ?></td>
 						<td><a href="<?php echo $qqhqUrl; ?>" target="_blank"><?php echo $stockInfo['code']; ?></a></td>
                         <?php if (isset($contMap[$sid])): ?>
@@ -49,10 +60,25 @@
 
 						<td><?php echo isset($priceMap[$sid])? $highTypeValues[$priceMap[$sid]['high_type']] : "-"; ?></td>
 						<td><?php echo $stockData['close_price']; ?></td>
+
+                        <?php if (empty($hqData)): ?>  
+						<td><?php echo "0.00"; ?></td>
+						<td><?php echo "0.00"; ?></td>
+						<td><?php echo "0.00"; ?></td>
 						<td><?php echo "-"; ?></td>
 						<td><?php echo "-"; ?></td>
-						<td><?php echo "-"; ?></td>
-						<td><?php echo "-"; ?></td>
+                        <?php else: ?>
+                            <?php $openPrice = $hqMap[$sid]['open_price']; ?>
+                            <?php $curPrice = $hqMap[$sid]['cur_price']; ?>
+
+                        <td class="<?php echo ($openPrice >= $stockData['close_price'])? 'red': 'green'; ?>"><?php echo sprintf("%.2f", $openPrice); ?></td>
+                        <td><?php echo sprintf("%.2f", $curPrice); ?></td>
+                        <td class="<?php echo ($curPrice >= $openPrice)? 'red': 'green'; ?>"><?php echo ($openPrice > 0)? round(($curPrice - $openPrice) / $openPrice * 100, 2) . "%" : "0.00%"; ?></td>
+
+                        <td><?php echo isset($hqMap[$sid]['trend'])? $hqMap[$sid]['trend']['trend'] : "-"; ?></td>
+                        <td class="<?php echo isset($hqMap[$sid]['trend']) && ($hqMap[$sid]['trend']['op'] == '买入')? 'red' : 'green'; ?>"><?php echo isset($hqMap[$sid]['trend'])? $hqMap[$sid]['trend']['op'] : "-"; ?></td>
+                            
+                        <?php endif; ?>
 					</tr>	
 					<?php endforeach; ?>
 				</tbody>
