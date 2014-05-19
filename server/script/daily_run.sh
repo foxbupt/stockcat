e2566c03bf43396a126a2ba3573a49518a87ad53
foxbupt <supervisedboy@gmail.com>
2014-05-19 22:00:41 +0800

    commit

--- server/script/daily_run.sh
+++ server/script/daily_run.sh
@@ -18,7 +18,7 @@ main()
     /usr/bin/python /home/fox/web/stockcat/server/lib/daily_refresh.py /home/fox/web/stockcat/server/lib/config.ini $day >> $result_path/refresh_highlow.log 
 
     # 深入分析
-    $PHP_BIN -c /etc/php.ini $WEB_PATH/console_entry.php analyze $day 5 >> $result_path/$log
+    $PHP_BIN -c /etc/php.ini $WEB_PATH/console_entry.php analyze $day 10 >> $result_path/$log
 
     echo "finish"
 }
