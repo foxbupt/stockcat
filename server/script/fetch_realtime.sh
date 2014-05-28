dfd683994467f4bb1acd63a587b7fe608e4680f5
foxbupt <supervisedboy@gmail.com>
2014-05-28 18:34:05 +0800

    commit

--- server/script/fetch_realtime.sh
+++ server/script/fetch_realtime.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#desc: 抓取股票每日总览数据
+#desc: 抓取股票实时交易数据
 #author: fox
 #date: 2013-08-12
 
 main()
 {
     day=`date "+%Y%m%d"`
-    interval=10
 
     if [ $# -ge 1 ]
     then
         day=$1
-    fi
-    if [ $# -ge 2 ]
-    then
-        interval=$2
     fi
 
     open=`is_market_open "$day"`
@@ -24,29 +19,22 @@ main()
         exit
     fi
 
-    cd $STOCK_SCRAPY_PATH
-    result_path=$STOCK_SCRAPY_PATH/data/$day
-    filename="poollist.txt"
-    log="daily_realtime_$day.log"
-    echo "log=$log filename=$filename"
+    slist=`/usr/local/bin/redis-cli -h 127.0.0.1 get "poollist-$day" | sed 's/"//g' | sed "s/{//g" | sed "s/}//g" | sed "s/,/\n/g"`
+    /usr/bin/python $SERVER_PATH/lib/fetch.py $SERVER_PATH/lib/config.ini "realtime" $slist
 
-    # 用scrapy抓取总览信息
-    count=0
-    /usr/local/bin/redis-cli -h 127.0.0.1 get "poollist-$day" | sed 's/"//g' | sed "s/{//g" | sed "s/}//g" | sed "s/,/\n/g" | sed "s/:/\t/g" |
-    while read line
-    do
-        sid=`echo $line | awk '{print $1}'`
-        scode=`echo $line | awk '{print $2}'`
-        echo "sid=$sid scode=$scode"
-        $SCRAPY_BIN crawl qq -a id=$sid -a code=$scode -a "start"=0930 -a redis_host=127.0.0.1 -a redis_port=6379 --logfile=$result_path/fetch_realtime.log >> $result_path/$log &
+    #do
+    #    sid=`echo $line | awk '{print $1}'`
+    #    scode=`echo $line | awk '{print $2}'`
+    #    echo "sid=$sid scode=$scode"
+    #    $SCRAPY_BIN crawl qq -a id=$sid -a code=$scode -a "start"=0930 -a redis_host=127.0.0.1 -a redis_port=6379 --logfile=$result_path/fetch_realtime.log >> $result_path/$log &
 
-        count=`expr $count + 1`
-        mod=`expr $count % $interval`
-        if [ $mod -eq 0 ]
-        then
-            sleep 10
-        fi
-    done #< $result_path/$filename
+    #    count=`expr $count + 1`
+    #    mod=`expr $count % $interval`
+    #    if [ $mod -eq 0 ]
+    #    then
+    #        sleep 10
+    #    fi
+    #done #< $result_path/$filename
 
     echo "finish"
 }
