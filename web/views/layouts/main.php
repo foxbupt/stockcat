7026332ff1d8a650574d832b064104bab150ae3c
foxbupt <huli@vip.qq.com>
2015-01-11 23:21:09 +0800

    no message

--- web/views/layouts/main.php
+++ web/views/layouts/main.php
@@ -72,9 +72,6 @@
 								<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 1)); ?>">快速拉升列表</a></li>
 								<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 0)); ?>">快速下跌列表</a></li>
 								<li><p class="navbar-text">|</p></li>
-								<li><a href="/news">资讯</a></li>
-								<li><a href="<?php echo Yii::app()->createUrl('/stock/report/list'); ?>">研报</a></li>
-								<li><a href="/faq">使用帮助</a></li>
 							</ul>
 							
 							<ul class="nav pull-right">
