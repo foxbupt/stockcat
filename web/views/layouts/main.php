204db980921f7721bab4cfaa93bcb9488aaa83e4
foxbupt <huli@vip.qq.com>
2016-04-26 18:25:41 +0800

    add nav menu

--- web/views/layouts/main.php
+++ web/views/layouts/main.php
@@ -29,59 +29,34 @@
 								
 								<li class="dropdown">
 								  <a class="dropdown-toggle" id="poolMenu" data-toggle="dropdown">
-								    	股票池 <span class="caret"></span>
+								    	A股 <span class="caret"></span>
 								  </a>
 								  <ul class="dropdown-menu" role="menu" >
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/index'); ?>">A股</a></li>
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/index', array('location' => CommonUtil::LOCATION_US)); ?>">美股</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/index'); ?>">股票池</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/realtime'); ?>">实时上涨列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rankList'); ?>">评级列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/threshold'); ?>">价格突破列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/upresist'); ?>">趋势突破列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/uplimit'); ?>">昨日涨停列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/candle'); ?>">蜡烛形态</a></li>
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 1)); ?>">快速拉升列表</a></li>
+									<li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 0)); ?>">快速下跌列表</a></li>
 								  </ul>
 								</li>
 								
-								<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/realtime'); ?>">实时上涨列表</a></li>
-								
-								<li class="dropdown">
-								  <a class="dropdown-toggle" data-toggle="dropdown">
-								    	评级列表 <span class="caret"></span>
-								  </a>
-								  <ul class="dropdown-menu" role="menu" >
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rankList'); ?>">A股</a></li>
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rankList', array('location' => CommonUtil::LOCATION_US)); ?>">美股</a></li>
-								  </ul>
-								</li>
-								
-								<li class="dropdown">
-								  <a class="dropdown-toggle" data-toggle="dropdown">
-								    	价格突破列表 <span class="caret"></span>
-								  </a>
-								  <ul class="dropdown-menu" role="menu" >
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/threshold'); ?>">A股</a></li>
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/threshold', array('location' => CommonUtil::LOCATION_US)); ?>">美股</a></li>
-								  </ul>
-								</li>
-								
-								<li class="dropdown">
-								  <a class="dropdown-toggle" data-toggle="dropdown">
-								    	趋势突破列表 <span class="caret"></span>
-								  </a>
-								  <ul class="dropdown-menu" role="menu" >
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/upresist'); ?>">A股</a></li>
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/upresist', array('location' => CommonUtil::LOCATION_US)); ?>">美股</a></li>
-								  </ul>
-								</li>
-								
-								<li class="dropdown">
-								  <a class="dropdown-toggle" data-toggle="dropdown">
-								    	昨日涨停列表 <span class="caret"></span>
-								  </a>
-								  <ul class="dropdown-menu" role="menu" >
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/uplimit'); ?>">A股</a></li>
-								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/uplimit', array('location' => CommonUtil::LOCATION_US)); ?>">美股</a></li>
-								  </ul>
-								</li>
-								
-								<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 1)); ?>">快速拉升列表</a></li>
-								<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 0)); ?>">快速下跌列表</a></li>
 								<li><p class="navbar-text">|</p></li>
+								
+								<li class="dropdown">
+								  <a class="dropdown-toggle" id="poolMenu" data-toggle="dropdown">
+								    	美股 <span class="caret"></span>
+								  </a>
+								  <ul class="dropdown-menu" role="menu" >
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/index', array('location' => CommonUtil::LOCATION_US)); ?>">股票池</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/rankList', array('location' => CommonUtil::LOCATION_US)); ?>">评级列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/threshold', array('location' => CommonUtil::LOCATION_US)); ?>">价格突破列表</a></li>
+								    <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo Yii::app()->createUrl('/stock/pool/upresist', array('location' => CommonUtil::LOCATION_US)); ?>">趋势突破列表</a></li>
+								  </ul>
+								</li>
 							</ul>
 							
 							<ul class="nav pull-right">
@@ -109,8 +84,6 @@
 						<h5>友情链接</h5>
 						<ul class="inline">
 							<li><a target="_blank" href="http://www.beva.com">贝瓦网</a></li>
-							<li><a target="_blank" href="http://www.pad4fun.com">平板之家</a></li>
-							<li><a target="_blank" href="http://www.ibibikan.com">早教比比看</a></li>
 						</ul>
 					</div>
 				</div>
--- web/views/site/index.php
+++ web/views/site/index.php
@@ -9,6 +9,7 @@
 		<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/uplimit'); ?>">昨日涨停列表</a></li>
 		<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 1)); ?>">快速拉升列表</a></li>
 		<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/rapid', array('rise' => 0)); ?>">快速下跌列表</a></li>
+		<li><a href="<?php echo Yii::app()->createUrl('/stock/pool/candle'); ?>">蜡烛形态</a></li>
 		<li><a href="<?php echo Yii::app()->createUrl('/stock/shortTerm/index'); ?>">短线追踪</a></li>
 		<li><a href="<?php echo Yii::app()->createUrl('/member/deal/own', array('location' => CommonUtil::LOCATION_CHINA)); ?>">我的持仓</a></li>
 		<li><a href="<?php echo Yii::app()->createUrl('/member/deal/history', array('location' => CommonUtil::LOCATION_CHINA)); ?>">历史交易记录</a></li>
